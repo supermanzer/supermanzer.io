b50bb0be47a90e9f7fc2f51bf3db6f90fe41ccf6
Ryan Manzer <ryan.manzer@gmail.com>
2023-09-30 08:44:41 -0700

    Adding site navigation header

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <h1>hello Nuxt 3</h1>
-  </div>
+  <SiteHeader />
+  <v-container>
+    <v-card color="blue lighten-3">
+      <v-card-title>Hello Nuxt!</v-card-title>
+    </v-card>
+  </v-container>
 </template>
---
+++ components/SiteHeader.vue
@@ -0,0 +1,44 @@
+<script setup>
+const route = { name: "index" }
+
+const title = "Supermanzer.io"
+
+const navigationItems = [
+  { name: "Home", href: "/", active: route.name == "index" },
+  { name: "Blog", href: "/blog", active: route.name.includes("blog") },
+  {
+    name: "Projects",
+    href: "/projects",
+    active: route.name.includes("projects"),
+  },
+  { name: "Photos", href: "/photos", active: route.name.includes("photos") },
+]
+</script>
+
+<template>
+  <div>
+    <v-toolbar
+      dark
+      prominent
+      class="text-white font-weight-black"
+      image="https://images.pexels.com/photos/97077/pexels-photo-97077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn
+          v-for="item in navigationItems"
+          :key="item.name"
+          :href="item.href"
+          :active="item.active"
+        >
+          {{ item.name }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </div>
+</template>
