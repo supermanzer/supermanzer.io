fdf5a4a6a97be242f07b5b10b05d4c15e478605a
supermanzer <ryan.manzer@gmail.com>
2024-11-30 10:35:50 -0800

    Updated body and header fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import * as sass from "sass";
 
 export default defineNuxtConfig({
   app: {
     // pageTransition: { name: 'page', mode: 'out-in' }
+    head: {
+      link: [
+        { rel: "preconect", href: "https://fonts.googleapis.com" },
+        { rel: "preconnect", href: "https://fonts.gstatic.com" },
+        { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet' }
+      ]
+    }
   },
 
   build: {
     transpile: ['vuetify'],
+  },
+
+  css: [
+    // 'assets/styles/main.scss'
+  ],
+  fonts: {
+    assets: {
+      prefix: '/_fonts/'
+    }
   },
 
   devtools: { enabled: true },
@@ -23,6 +41,7 @@ export default defineNuxtConfig({
     '@nuxt/image',
     'nuxt-icon',
     '@nuxt/eslint',
+    '@nuxt/fonts',
     // 'nuxt-graphql-client',
     // '@pinia/nuxt',
     (_options, nuxt) => {
@@ -80,8 +99,17 @@ export default defineNuxtConfig({
     css: {
       preprocessorOptions: {
         scss: {
-          api: 'modern-compiler'
-        }
+          api: "modern",
+          importers: [
+            new sass.NodePackageImporter()
+          ]
+        },
+        sass: {
+          api: "modern",
+          importers: [
+            new sass.NodePackageImporter()
+          ]
+        },
       }
     }
   },
--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,7 +1,7 @@
 // import this after install `@mdi/font` package
 import '@mdi/font/css/materialdesignicons.css'
 
-import 'vuetify/styles'
+import '@/assets/styles/main.scss'
 import { createVuetify } from 'vuetify'
 import { md1 } from 'vuetify/blueprints'
 
